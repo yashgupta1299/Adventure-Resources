f28fe4b0109ebcf1c40cf87c98b0c64f0cc98dde
yashgupta1299 <86516989+yashgupta1299@users.noreply.github.com>
2022-12-22 21:37:11 +0530

    update

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
-        message: 'hello all',
-        app: 'natour',
+        status: 'success',
+        results: tours.length,
+        data: { tours },
     });
 });
-app.post('/', (req, res) => {
-    res.send('you can post to this end point');
+
+app.post('/api/v1/tours', (req, res) => {
+    const newID = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newID }, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            },
+        });
+    });
 });
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`listening to port ${port}..`);
