4efd4f836c67a5217ded168cc12cd0bc8eff4ee8
yashgupta1299 <86516989+yashgupta1299@users.noreply.github.com>
2022-12-31 23:59:33 +0530

    create the tour detail page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -12,8 +12,17 @@ exports.getOverview = catchAsync(async (req, res) => {
         tours
     });
 });
-exports.getTour = (req, res) => {
-    res.status(200).render('tour', {
-        title: 'forest hiker  '
+exports.getTour = catchAsync(async (req, res) => {
+    // get the data, for requested tour (including reviews and guides)
+    const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+        path: 'reviews',
+        field: 'review rating user'
     });
-};
+
+    //2 build template
+    //3 Render that template using tour data
+    res.status(200).render('tour', {
+        title: 'The Forest Hiker Tour',
+        tour
+    });
+});
